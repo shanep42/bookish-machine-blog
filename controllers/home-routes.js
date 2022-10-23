47947681f3a6b59e83b7d14d1ddd5d38436e7798
Shane <shanep42@gmail.com>
2022-10-23 14:41:35 -0500

    Initial commit with starter code from class exercise

---
+++ controllers/home-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { Gallery, Painting } = require('../models');
+// Import the custom middleware
+const withAuth = require('../utils/auth');
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- Active: 1664840661577@@127.0.0.1@3306@books_db
+
+DROP DATABASE IF EXISTS blog_db;
+
+CREATE DATABASE blog_db;
+
+USE blog_db;
